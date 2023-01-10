25c485f77205524aa79c01f05bd9cd133e171b0c
FRA2721 <cimino.francesco36@gmail.com>
2023-01-10 19:16:36 +0100

    /Admin check

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/admin'; ///dashboard changed to /admin value
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
