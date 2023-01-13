7258871d3276aa70250b6fbfe3c135959c4ecb21
FRA2721 <cimino.francesco36@gmail.com>
2023-01-13 01:08:13 +0100

    Add: destroy

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -95,7 +95,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        // $post->delete();
-        // return redirect()->route("admin.posts.index");
+        $post->delete();
+        return redirect()->route("admin.posts.index");
     }
 }
